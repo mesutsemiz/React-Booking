d50cfc99daf55d3864e1ad5524d23f6cfaa4c3f3
mesutsemiz <semihturgutlu@gmail.com>
2022-06-02 15:06:24 +0300

    header component is finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './style.css';
-import Home from "./pages/home/Home"
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
+
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/hotels" element={<List/>} />
+        <Route path="/hotels/:id" element={<Hotel/>} />
+        
       </Routes>
     </BrowserRouter>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react"
+import React from "raect";
+import Navbar from "../../components/navbar/Navbar"
+import "./home.css"
+import Header from "../header/Header";
 
 
 const Home=()=>{
   return(
     <div>
-      home
+      <Navbar/>
+      <Header/>
+      <div className="homeContainer">
+        
+      </div>
     </div>
   )
 }
